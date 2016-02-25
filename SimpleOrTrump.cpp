9e2b05bf2664058c709f00f67442fcf1ccf47be5
sugarfreeoh <asweet1983@gmail.com>
2016-02-25 06:18:48 -0500

    Add functions to parse the input

--- SimpleOrTrump.cpp
+++ SimpleOrTrump.cpp
@@ -3,7 +3,7 @@
  * Author:          sugarfreeoh
  * Date:            2016 Feb 25
  * Description:     First argument is a path to a file. Each line
- *                  includes a test casse which contains two playing
+ *                  includes a test case which contains two playing
  *                  cards and a trump suit. Cards and a trump suit
  *                  are separated by '|'. The card deck includes 13
  *                  ranks from four suits.
@@ -16,12 +16,18 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+
+void ParseLine(string inputLine, vector<string> &cards, char delimiter);
+void ParsePlayingCards(string inputLine, vector<string> &cards, char delimiter);
 
 int main(int argc, char *argv[])
 {
     ifstream inputFile;
+
+    char delimiter = '|';
 
     inputFile.open(argv[1]);
 
@@ -31,7 +37,17 @@ int main(int argc, char *argv[])
 
         while(getline(inputFile,line))
         {
+            vector<string> cards;
             cout << "Input: " << line << endl;
+
+            ParseLine(line,cards,delimiter);
+
+            for(vector<string>::const_iterator it=cards.begin();
+                it!=cards.end();
+                ++it)
+            {
+                cout << *it << endl;
+            }
         }
     }
     else
@@ -40,4 +56,46 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+
+void ParseLine(string inputLine, vector<string> &cards, char delimiter)
+{
+    stringstream lineStream;
+
+    string temp;
+
+    bool separatedHands = false;
+
+    char cardDelimiter = ' ';
+
+    lineStream << inputLine;
+
+    while(getline(lineStream,temp,delimiter))
+    {
+        if(separatedHands == false)
+        {
+            ParsePlayingCards(temp.substr(0,temp.size()-1),cards,cardDelimiter);
+
+            separatedHands = true;
+        }
+        else
+        {
+            cards.push_back(temp.substr(1,temp.size()-1));
+        }   
+    }
+}
+
+void ParsePlayingCards(string inputLine, vector<string> &cards, char delimiter)
+{
+    stringstream lineStream;
+
+    string temp;
+
+    lineStream << inputLine;
+
+    while(getline(lineStream,temp,' '))
+    {
+        cards.push_back(temp);
+    }
 }
